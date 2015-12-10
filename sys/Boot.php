1b90599a86b7ce320f65efc636983679390e3abd
Kerem Gunes <qeremy@gmail.com>
2015-12-10 20:22:44 +0200

    add bootstrap file.

--- sys/Boot.php
+++ sys/Boot.php
@@ -1,1 +1,74 @@
 <?php
+/** !!! MUST BE CALLED FIRST HERE ONCE !!! **/
+ob_start();
+
+/**
+ * Application constants.
+ * @const bool, bool, float
+ */
+define('APP_TEST', true);
+define('APP_DEBUG', true);
+define('APP_START_TIME', microtime(true));
+
+/**
+ * Just for fun.
+ * @const null
+ */
+define('nil', null, true);
+
+/**
+ * These defs stand here cos of PHP's typehint leak. So, you can use these
+ * instead of NULL when defining as defult params like foo(bool $x = NULL).
+ * @const bool, bool, string, float
+ */
+define('True', true);
+define('False', false);
+define('None', '', true);
+
+/**
+ * Application root path.
+ * @const string
+ */
+define('root', __dir__ .'/..', true);
+
+/**
+ * Used to detect development environment.
+ * @const bool
+ */
+define('local', ('.local' == substr($_SERVER['SERVER_NAME'], -6)), true);
+
+/**
+ * HTTP/HTTPS constants.
+ * @const string
+ */
+define('http', 'http://'. $_SERVER['SERVER_NAME'], true);
+define('https', 'https://'. $_SERVER['SERVER_NAME'], true);
+
+/**
+ * Error settings.
+ */
+if (local /* or admin? */) {
+    ini_set('display_errors', 1);
+    ini_set('error_reporting', E_ALL);
+}
+
+/**
+ * Load global base files.
+ */
+require(root.'/sys/global/def.php');
+require(root.'/sys/global/cfg.php');
+require(root.'/sys/global/fun.php');
+
+/** !!! START APPLICATION !!! **/
+// autoload
+$autoload = require(root .'/sys/library/class/Autoload.php');
+$autoload->register();
+
+// // init application
+// $app = Shobbr\Application::init();
+
+// // set system defaults (locale, encoding etc)
+// $app->setDefaults();
+
+// // return application
+// return $app;
