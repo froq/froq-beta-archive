d4e957fb9f215060d8f61ddf39394ae42f96092b
Kerem Gunes <qeremy@gmail.com>
2015-12-10 22:53:54 +0200

    rename prepareName() to toName().

--- sys/library/class/Application/Service/Service.php
+++ sys/library/class/Application/Service/Service.php
@@ -6,10 +6,10 @@ final class Service
     private $name;
 
     final public function __construct($name = null) {
-        $this->name = $this->prepareName($name);
+        $this->name = $this->toName($name);
     }
 
-    final public function prepareName($name) {
+    final public function toName($name) {
         return preg_replace_callback('~-([a-z])~i', function($match) {
             return ucfirst($match[1]);
         }, ucfirst($name));
