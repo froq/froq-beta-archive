3550e3625fde3697ff84c2ada396b92cb772ea3c
Kerem Gunes <qeremy@gmail.com>
2015-12-10 22:50:41 +0200

    modify name set.

--- sys/library/class/Application/Service/Service.php
+++ sys/library/class/Application/Service/Service.php
@@ -6,12 +6,11 @@ final class Service
     private $name;
 
     final public function __construct($name = null) {
-        $this->name = $name;
+        $this->name = $this->prepareName($name);
     }
 
     final public function prepareName($name) {
         return preg_replace_callback('~-([a-z])~i', function($match) {
-            pre($match);
             return ucfirst($match[1]);
         }, ucfirst($name));
     }
