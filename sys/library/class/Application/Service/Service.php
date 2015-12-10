ceeb1d8168361cd5809ed70125a35ba22bc4d520
Kerem Gunes <qeremy@gmail.com>
2015-12-10 22:50:13 +0200

    add $name, __construct(), prepareName().

--- sys/library/class/Application/Service/Service.php
+++ sys/library/class/Application/Service/Service.php
@@ -2,4 +2,17 @@
 
 final class Service
     implements ServiceInterface
-{}
+{
+    private $name;
+
+    final public function __construct($name = null) {
+        $this->name = $name;
+    }
+
+    final public function prepareName($name) {
+        return preg_replace_callback('~-([a-z])~i', function($match) {
+            pre($match);
+            return ucfirst($match[1]);
+        }, ucfirst($name));
+    }
+}
