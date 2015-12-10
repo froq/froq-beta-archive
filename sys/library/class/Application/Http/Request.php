7312c02ea37709a564846a2f5332fb5e8749bf8a
Kerem Gunes <qeremy@gmail.com>
2015-12-11 00:51:19 +0200

    add Application\Http\Request object.

---
+++ sys/library/class/Application/Http/Request.php
@@ -0,0 +1,9 @@
+<?php namespace Application\Http;
+
+final class Request
+{
+    const METHOD_GET    = 'GET',
+          METHOD_POST   = 'POST',
+          METHOD_UPDATE = 'UPDATE',
+          METHOD_DELETE = 'DELETE';
+}
