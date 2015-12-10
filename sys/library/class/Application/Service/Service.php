1d94934071fbf6e2d5b8f825c382131c1d6fc016
Kerem Gunes <qeremy@gmail.com>
2015-12-10 22:39:07 +0200

    add Application\Service objects.

---
+++ sys/library/class/Application/Service/Service.php
@@ -0,0 +1,5 @@
+<?php namespace Application\Service;
+
+final class Service
+    implements ServiceInterface
+{}
---
+++ sys/library/class/Application/Service/ServiceInterface.php
@@ -0,0 +1,4 @@
+<?php namespace Application\Service;
+
+interface ServiceInterface
+{}
