cbd7a03cf51c1a111f84ab952a0391cd064a973c
Kerem Gunes <qeremy@gmail.com>
2015-12-10 20:55:36 +0200

    add Application\Util\Traits\SingleTrait object.

---
+++ sys/library/class/Application/Util/Traits/SingleTrait.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright (c) 2015 Hazır Tur
+ *    <http://hazirtur.com>
+ */
+namespace Application\Util\Traits;
+
+/**
+ * @package    Application
+ * @subpackage Application\Util\Traits
+ * @object     Application\Util\Traits\SingleTrait
+ * @author     Kerem! <qeremy@gmail>
+ *
+ * Notice: Do not define `__construct` or `__clone`
+ * methods as public if you wanna single user object.
+ */
+trait SingleTrait
+{
+    /**
+     * Instance holder.
+     * @var array
+     */
+    private static $__instances = [];
+
+    /**
+     * Forbid idle initializations.
+     */
+    private function __clone() {}
+    private function __construct() {}
+
+    /**
+     * Constructor.
+     *
+     * @return object
+     */
+    final public static function init() {
+        $className = get_called_class();
+        if (!isset(self::$__instances[$className])) {
+            // late-static-bound class name (that user subclass)
+            self::$__instances[$className] = new static();
+        }
+
+        return self::$__instances[$className];
+    }
+}
+
