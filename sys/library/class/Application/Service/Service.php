977ac1e12d5df03593e07422327ff5784276dd09
Kerem Gunes <qeremy@gmail.com>
2015-12-11 03:38:55 +0200

    extend Application|\Service\Service.

--- sys/library/class/Application/Service/Service.php
+++ sys/library/class/Application/Service/Service.php
@@ -1,30 +1,85 @@
 <?php namespace Application\Service;
 
+use \Application\Exception;
 use \Application\Http\Uri\UriPath;
 
 abstract class Service
     implements ServiceInterface
 {
+    protected $app;
     protected $name;
-    protected $allowedMethods = [];
+    protected $method;
+    protected $uriPath;
+    protected $requestMethods = [];
 
-    final public function isMethodAllowed($method) {
-        return in_array($method, $this->allowedMethods);
+    public function __construct($name = null) {
+        $this->name = $name;
     }
 
-    final public function setAllowedMethods(...$allowedMethods) {
-        $this->allowedMethods = $allowedMethods;
+    final public function isRequestMethodAllowed($requestMethod) {
+        return in_array($requestMethod, $this->requestMethods);
     }
-    final public function getAllowedMethods() {
-        return $this->allowedMethods;
+
+    final public function isHome() {
+        return ($this->method == '');
+    }
+
+    final public function callMethod($method, $halt = true) {
+        if (method_exists($this, $method)) {
+            return $this->{$method}();
+        }
+        if ($halt) {
+            throw new Exception(sprintf('`%s` method not found on `%s`',
+                $method, get_called_class()));
+        }
+    }
+    final public function callMethodInit() {
+        return $this->callMethod(ServiceInterface::DEFAULT_METHOD_INIT, false);
+    }
+    final public function callMethodHome() {
+        return $this->callMethod(ServiceInterface::DEFAULT_METHOD_HOME);
+    }
+    final public function callMethodInvoked() {
+        return $this->callMethod($this->method);
+    }
+
+    final public function setApp($app) {
+        $this->app = $app;
+        return $this;
+    }
+    final public function getApp() {
+        return $this->app;
     }
 
     final public function setName($name) {
         $this->name = $name;
+        return $this;
     }
     final public function getName() {
         return $this->name;
     }
 
-    // final
+    final public function setMethod($method) {
+        $this->method = $method;
+        return $this;
+    }
+    final public function getMethod() {
+        return $this->method;
+    }
+
+    final public function setUriPath(UriPath $uriPath) {
+        $this->uriPath = $uriPath;
+        return $this;
+    }
+    final public function getUriPath() {
+        return $this->uriPath;
+    }
+
+    final public function setRequestMethods(...$requestMethods) {
+        $this->requestMethods = $requestMethods;
+        return $this;
+    }
+    final public function getRequestMethods() {
+        return $this->requestMethods;
+    }
 }
