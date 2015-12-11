1fbb542a3d43c3aadab05294792020a808301475
Kerem Gunes <qeremy@gmail.com>
2015-12-11 02:59:09 +0200

    trivial.

--- sys/library/class/Application/Service/ServiceInterface.php
+++ sys/library/class/Application/Service/ServiceInterface.php
@@ -3,8 +3,8 @@
 interface ServiceInterface
 {
     const DEFAULT_NAME = 'HomeService';
-    const DEFAULT_METHOD_HOME = '__home__';
     const DEFAULT_METHOD_INIT = '__init__';
+    const DEFAULT_METHOD_HOME = '__home__';
 
     public function __home__();
 }
