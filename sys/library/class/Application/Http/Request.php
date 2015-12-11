31414d88f02770e30205fd565e14c5125eca2945
Kerem Gunes <qeremy@gmail.com>
2015-12-11 15:17:02 +0200

    add Application\Http\Request object.

--- sys/library/class/Application/Http/Request.php
+++ sys/library/class/Application/Http/Request.php
@@ -1,9 +1,25 @@
 <?php namespace Application\Http;
 
+use \Application\Http\Uri\Uri;
+
 final class Request
 {
     const METHOD_GET    = 'GET',
           METHOD_POST   = 'POST',
           METHOD_UPDATE = 'UPDATE',
           METHOD_DELETE = 'DELETE';
+
+    private $method;
+    private $uri;
+    private $params;
+
+    final public function __construct() {
+        // set method
+        $this->method = strtoupper($_SERVER['REQUEST_METHOD']);
+
+        // set uri
+        $this->uri = new Uri(
+            'http'. (($_SERVER['SERVER_PORT'] == '443') ? 's' : '')
+                .'://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
+    }
 }
