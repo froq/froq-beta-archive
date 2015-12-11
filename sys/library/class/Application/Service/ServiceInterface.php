87483258c5f363961fbf3e4672963b706f73a998
Kerem Gunes <qeremy@gmail.com>
2015-12-11 02:49:13 +0200

    remove required init(), add defaults.

--- sys/library/class/Application/Service/ServiceInterface.php
+++ sys/library/class/Application/Service/ServiceInterface.php
@@ -2,6 +2,9 @@
 
 interface ServiceInterface
 {
-    public function init();
+    const DEFAULT_NAME = 'HomeService';
+    const DEFAULT_METHOD_HOME = 'home';
+    const DEFAULT_METHOD_INIT = 'init';
+
     public function home();
 }
