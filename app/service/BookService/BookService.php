0a7d6b919e8c2d97912df723ae8f9b3fa92ba26e
Kerem Gunes <qeremy@gmail.com>
2015-12-12 02:07:33 +0200

    mock-up.

--- app/service/BookService/BookService.php
+++ app/service/BookService/BookService.php
@@ -1,31 +1,19 @@
 <?php defined('root') or die('Access denied!');
 
-use \Application\Http\Request;
-use \Application\Service\Service;
+use Application\Http\Request;
+use Application\Service\Service;
 
 class BookService extends Service
 {
     protected $mainOnly = true;
     protected $allowedRequestMethods = [Request::METHOD_GET, Request::METHOD_POST];
 
-    // public function _init() {
-    //     pre($this->config);
-    // }
+    public function _init() {
+        $this->model = new BookModel();
+    }
 
     public function _main() {
-        $this->printId();
-    }
-
-    public function _before() {
-        pre(__method__);
-    }
-    public function _after() {
-        pre(__method__);
-    }
-
-    private function printId() {
-        $id = $this->app->request->uri->segment(1);
-        print $id;
+        $this->model->id = 1;
+        pre($this->model);
     }
 }
-
