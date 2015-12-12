1edc7bff4787f96d685e74da50c2ecd30919b31f
Kerem Gunes <qeremy@gmail.com>
2015-12-13 00:10:54 +0200

    update for global service view files.

--- sys/library/class/Application/Application/View.php
+++ sys/library/class/Application/Application/View.php
@@ -14,23 +14,42 @@ final class View
     }
 
     final public function display($file, array $data = null) {
-        $file = $this->prepareFile($file);
-
-        extract((array) $data);
-
-        return include($file);
+        $this->includeFile($this->prepareFile($file), $data);
     }
 
     final public function displayHead(array $data = null) {
-        $this->display(self::PARTIAL_HEAD, $data);
+        // check local service file
+        $file = $this->prepareFile(self::PARTIAL_HEAD, false);
+        if (!is_file($file)) {
+            // look up for global service file
+            $file = $this->prepareFileGlobal(self::PARTIAL_HEAD);
+        }
+        $this->includeFile($file, $data);
     }
     final public function displayFoot(array $data = null) {
-        $this->display(self::PARTIAL_FOOT, $data);
+        // check local service file
+        $file = $this->prepareFile(self::PARTIAL_FOOT, false);
+        if (!is_file($file)) {
+            // look up for global service file
+            $file = $this->prepareFileGlobal(self::PARTIAL_FOOT);
+        }
+        $this->includeFile($file, $data);
     }
 
-    final public function prepareFile($file) {
+    final public function includeFile($file, array $data = null) {
+        extract((array) $data);
+        include($file);
+    }
+    final public function prepareFile($file, $fileCheck = true) {
         $file = sprintf('./app/service/%s/view/%s.php', $this->service->getName(), $file);
-        if (!is_file($file)) {
+        if ($fileCheck && !is_file($file)) {
+            throw new \RuntimeException('View file not found! file: '. $file);
+        }
+        return $file;
+    }
+    final public function prepareFileGlobal($file, $fileCheck = true) {
+        $file = sprintf('./app/service/view/%s.php', $file);
+        if ($fileCheck && !is_file($file)) {
             throw new \RuntimeException('View file not found! file: '. $file);
         }
         return $file;
