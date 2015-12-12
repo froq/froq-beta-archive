3b0d7fafc56664e2ff08141251c6ca5b5f16b03b
Kerem Gunes <qeremy@gmail.com>
2015-12-12 02:02:43 +0200

    add Database contents.

--- sys/library/class/Application/Database/Database.php
+++ sys/library/class/Application/Database/Database.php
@@ -1,1 +1,43 @@
-<?php
+<?php namespace Application\Database;
+
+use Application\Application;
+use Application\Exception;
+use Application\Database\Vendor\Mysql,
+    Application\Database\Vendor\Couch,
+    Application\Database\Vendor\Mongo;
+
+final class Database
+{
+    const VENDOR_MYSQL = 'mysql',
+          VENDOR_COUCH = 'couch',
+          VENDOR_MONGO = 'mongo';
+
+    private static $instances = [];
+
+    final public function __construct() {}
+
+    final public function model($name) {
+        return new $name();
+    }
+
+    final public static function init($vendor) {
+        if (isset(self::$instances[$vendor])) {
+            return self::$instances[$vendor];
+        }
+        $app = app();
+        $db = null;
+        $dbConfig = $app->config->get('db');
+        switch ($vendor) {
+            case self::VENDOR_MYSQL:
+                if (!isset($dbConfig[self::VENDOR_MYSQL])) {
+                    throw new Exception('MySQL options not found in config!');
+                }
+                $db = new Mysql($dbConfig);
+                break;
+            default:
+                throw new Exception('Unimplemented vendor given!');
+        }
+
+        return (self::$instances[$vendor] = $db);
+    }
+}
