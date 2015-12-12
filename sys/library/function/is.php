f460cfc045ee645cb638baf4f798f8af8d67a9c5
Kerem Gunes <qeremy@gmail.com>
2015-12-12 02:41:21 +0200

    add is module.

---
+++ sys/library/function/is.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Is function module.
+ */
+
+/**
+ * Check env is local.
+ * @return bool
+ */
+function is_local() {
+    if (defined('local')) {
+        return (local === true);
+    }
+    return ((bool) strstr($_SERVER['SERVER_NAME'], '.local'));
+}
