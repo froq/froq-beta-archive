a2d6edcfbd6ae8088e69d3d2e8de772a601c7bdf
Kerem Gunes <qeremy@gmail.com>
2015-12-12 02:10:05 +0200

    add Vendor.

---
+++ sys/library/class/Application/Database/Vendor/Mysql.php
@@ -0,0 +1,10 @@
+<?php namespace Application\Database\Vendor;
+
+final class Mysql
+{
+    private $db;
+
+    final public function __construct(array $config) {
+        $this->db = "new oppa";
+    }
+}
