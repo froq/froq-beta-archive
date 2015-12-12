99b80864d5465312cbce29d2550469a71f9fe333
Kerem Gunes <qeremy@gmail.com>
2015-12-12 05:08:02 +0200

    add initor.

--- sys/library/class/Application/Database/Vendor/Mysql.php
+++ sys/library/class/Application/Database/Vendor/Mysql.php
@@ -1,10 +1,18 @@
 <?php namespace Application\Database\Vendor;
 
+use Application\Util\Traits\SingleTrait;
+use Oppa\Configuration,
+    Oppa\Database\Query,
+    Oppa\Database\Factory;
+
 final class Mysql
 {
+    use SingleTrait;
+
     private $db;
 
-    final public function __construct(array $config) {
-        $this->db = "new oppa";
+    final private function __construct(array $config) {
+        $this->db = Factory::build(new Configuration($config));
+        $this->db->connect();
     }
 }
