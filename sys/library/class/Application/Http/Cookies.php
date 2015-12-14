2b9ae71e56cf497d2d231439150f90dc2df21261
Kerem Gunes <qeremy@gmail.com>
2015-12-14 03:21:54 +0200

    add Application\Http\Cookies.

---
+++ sys/library/class/Application/Http/Cookies.php
@@ -0,0 +1,24 @@
+<?php
+namespace Application\Http;
+
+use Application\Util\Collection;
+
+/**
+ * @package    Application
+ * @subpackage Application\Http
+ * @object     Application\Http\Cookies
+ * @extends    Application\Util\Collection
+ * @author     Kerem! <qeremy@gmail>
+ */
+final class Cookies
+    extends Collection
+{
+    /**
+     * Object constructor.
+     *
+     * @param array $cookies
+     */
+    final public function __construct(array $cookies = []) {
+        parent::__construct($cookies);
+    }
+}
