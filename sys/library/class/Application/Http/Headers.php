31ae9f9dbf7610484f5c6ca6811c62af83f2e4a2
Kerem Gunes <qeremy@gmail.com>
2015-12-14 03:22:04 +0200

    add Application\Http\Headers.

---
+++ sys/library/class/Application/Http/Headers.php
@@ -0,0 +1,24 @@
+<?php
+namespace Application\Http;
+
+use Application\Util\Collection;
+
+/**
+ * @package    Application
+ * @subpackage Application\Http
+ * @object     Application\Http\Headers
+ * @extends    Application\Util\Collection
+ * @author     Kerem! <qeremy@gmail>
+ */
+final class Headers
+    extends Collection
+{
+    /**
+     * Object constructor.
+     *
+     * @param array $headers
+     */
+    final public function __construct(array $headers = []) {
+        parent::__construct($headers);
+    }
+}
