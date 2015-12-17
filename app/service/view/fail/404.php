100f4c5a23b6b224c3e5e04e726051a106238769
Kerem Gunes <qeremy@gmail.com>
2015-12-18 00:58:36 +0200

    update fial files.

--- app/service/view/fail/404.php
+++ app/service/view/fail/404.php
@@ -1,5 +1,4 @@
 <?php defined('root') or die('Access denied!'); ?>
 
-<h1><?php print $error; ?></h1>
-
+<h1>404 Not Found</h1>
 <h3><?php print $error_detail; ?></h3>
--- app/service/view/fail/main.php
+++ app/service/view/fail/main.php
@@ -2,12 +2,12 @@
 
 <?php if (isset($error, $error_detail)): ?>
 
-<h1><?php print $error; ?></h1>
-<h3><?php print $error_detail; ?></h3>
+    <h1><?php print $error; ?></h1>
+    <h3><?php print $error_detail; ?></h3>
 
 <?php else: ?>
 
-<h1>Error!</h1>
-<h3>Unknown error occurred.</h3>
+    <h1>Error!</h1>
+    <h3>Unknown error occurred.</h3>
 
 <?php endif; ?>
