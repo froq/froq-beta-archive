ffcca731b15aaa9d5c782c00ced0378689700733
Kerem Gunes <qeremy@gmail.com>
2015-12-17 06:44:55 +0200

    add view/fail/{main,404}.php files.

---
+++ app/service/view/fail/404.php
@@ -0,0 +1,5 @@
+<?php defined('root') or die('Access denied!'); ?>
+
+<h1><?php print $error; ?></h1>
+
+<h3><?php print $error_detail; ?></h3>
---
+++ app/service/view/fail/main.php
@@ -0,0 +1,13 @@
+<?php defined('root') or die('Access denied!'); ?>
+
+<?php if (isset($error, $error_detail)): ?>
+
+<h1><?php print $error; ?></h1>
+<h3><?php print $error_detail; ?></h3>
+
+<?php else: ?>
+
+<h1>Error!</h1>
+<h3>Unknown error occurred.</h3>
+
+<?php endif; ?>
