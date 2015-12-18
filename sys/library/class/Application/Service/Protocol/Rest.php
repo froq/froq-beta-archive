b92135006168f952839dfe197bbab89b4aaf7756
Kerem Gunes <qeremy@gmail.com>
2015-12-18 02:54:43 +0200

    add put().

--- sys/library/class/Application/Service/Protocol/Rest.php
+++ sys/library/class/Application/Service/Protocol/Rest.php
@@ -3,10 +3,12 @@ namespace Application\Service\Protocol;
 
 use Application\Service\Service;
 
+// https://spring.io/understanding/REST
 abstract class Rest extends Service
 {
     abstract public function get();
     abstract public function post();
+    abstract public function put();
     abstract public function patch();
     abstract public function delete();
 }
