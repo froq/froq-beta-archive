f537f5eba73cad61685ae5fdbc8c4027ead2b01b
Kerem Gunes <qeremy@gmail.com>
2015-12-18 04:06:16 +0200

    add to module.

---
+++ sys/library/function/to.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/**
+ * To function module.
+ */
+
+/**
+ * Convert an iterable to array.
+ * @param  iter $input
+ * @param  bool $deep
+ * @return array
+ */
+function to_array($input, bool $deep = true): array {
+    $input = (array) $input;
+    if ($deep) {
+        foreach ($input as $key => $value) {
+            $input[$key] = is_iter($value)
+                ? to_array($value, $deep) : $value;
+        }
+    }
+    return $input;
+}
+
+/**
+ * Convert an iterable to object.
+ * @param  iter $input
+ * @param  bool $deep
+ * @return array
+ */
+function to_object($input, bool $deep = true): \stdClass {
+    $input = (object) $input;
+    if ($deep) {
+        foreach ($input as $key => $value) {
+            $input->{$key} = is_iter($value)
+                ? to_object($value, $deep) : $value;
+        }
+    }
+    return $input;
+}
