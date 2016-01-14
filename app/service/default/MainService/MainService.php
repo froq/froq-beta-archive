fab5b2923e8e26bf9a539b5ff36a336c94d32c80
Kerem Güneş <qeremy@gmail.com>
2016-01-14 02:59:55 +0200

    remove required init().

--- app/service/default/MainService/MainService.php
+++ app/service/default/MainService/MainService.php
@@ -4,15 +4,8 @@ use Application\Service\Protocol\Site as Service;
 
 class MainService extends Service
 {
-    public function init() {
-        // if (user logged in) {
-        //     redirect /home
-        // } else {
-        //     redirect /account/login
-        // }
-    }
-
-    public function main(): string {
+    public function main(): string
+    {
         return 'Hello, world!';
     }
 }
