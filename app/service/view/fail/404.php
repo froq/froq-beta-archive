184209dbd894819f3b73433a5f6837e244a58866
Kerem Güneş <qeremy@gmail.com>
2016-01-14 03:43:01 +0200

    trivial.

--- app/service/view/fail/404.php
+++ app/service/view/fail/404.php
@@ -1,4 +1,4 @@
 <?php defined('root') or die('Access denied!'); ?>
 
 <h1>404 Not Found</h1>
-<h3><?php print $error_detail; ?></h3>
+<p><?php print $error_detail; ?></p>
--- app/service/view/fail/main.php
+++ app/service/view/fail/main.php
@@ -3,11 +3,11 @@
 <?php if (isset($error, $error_detail)): ?>
 
     <h1><?php print $error; ?></h1>
-    <h3><?php print $error_detail; ?></h3>
+    <p><?php print $error_detail; ?></p>
 
 <?php else: ?>
 
     <h1>Error!</h1>
-    <h3>Unknown error occurred.</h3>
+    <p>Unknown error occurred.</p>
 
 <?php endif; ?>
