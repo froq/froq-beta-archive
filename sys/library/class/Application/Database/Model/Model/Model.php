7305399fbb3898513c63827703120cc0d34f4ab4
Kerem Güneş <qeremy@gmail.com>
2016-01-21 00:11:13 +0200

    format (use 3-tabs).

--- sys/library/class/Application/Database/Model/Model/Model.php
+++ sys/library/class/Application/Database/Model/Model/Model.php
@@ -5,37 +5,37 @@ use Application\Database\Model\ModelInterface;
 
 abstract class Model implements ModelInterface
 {
-    protected $db;
-    protected $name;
-    protected $primary;
+   protected $db;
+   protected $name;
+   protected $primary;
 
-    final public function setDb($db): self
-    {
-        $this->db = $db;
-        return $this;
-    }
-    final public function getDb()
-    {
-        return $this->db;
-    }
+   final public function setDb($db): self
+   {
+      $this->db = $db;
+      return $this;
+   }
+   final public function getDb()
+   {
+      return $this->db;
+   }
 
-    final public function setName($name): self
-    {
-        $this->name = $name;
-        return $this;
-    }
-    final public function getName(): string
-    {
-        return $this->name;
-    }
+   final public function setName($name): self
+   {
+      $this->name = $name;
+      return $this;
+   }
+   final public function getName(): string
+   {
+      return $this->name;
+   }
 
-    final public function setPrimary($primary): self
-    {
-        $this->primary = $primary;
-        return $this;
-    }
-    final public function getPrimary(): string
-    {
-        return $this->primary;
-    }
+   final public function setPrimary($primary): self
+   {
+      $this->primary = $primary;
+      return $this;
+   }
+   final public function getPrimary(): string
+   {
+      return $this->primary;
+   }
 }
