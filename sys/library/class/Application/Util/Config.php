02101dbc7359d6c64fd793ed220fa9681a0626a0
Kerem Güneş <qeremy@gmail.com>
2016-01-20 03:43:26 +0200

    format (use 3-tabs).

--- sys/library/class/Application/Util/Config.php
+++ sys/library/class/Application/Util/Config.php
@@ -1,45 +1,59 @@
 <?php namespace Application\Util;
 
-final class Config extends Collection
+final class Config
+   extends Collection
 {
-    final public function __construct($data, $merge = false) {
-        if (is_string($data)) {
-            $data = require($data);
-        }
-        if (!is_array($data)) {
-            throw new \RuntimeException(
-                'Config data must be array or path to array file!');
-        }
-        if ($merge) {
-            $data = self::merge($this->data, $data);
-        }
-        $this->setData($data);
-    }
+   final public function __construct($data, $merge = false) {
+      if (is_string($data)) {
+         $data = require($data);
+      }
 
-    final public function set($key, $value) {
-        $this->data[$key] = $value;
-        return $this;
-    }
-    final public function get($key, $valueDefault = null) {
-        return dig($this->data, $key, $valueDefault);
-    }
+      if (!is_array($data)) {
+         throw new \RuntimeException(
+            'Config data must be array or path to array file!');
+      }
 
-    final public static function merge(array $source, array $target) {
-        foreach ($source as $key => $value) {
-            if (isset($target[$key]) && is_array($value)) {
-                $target[$key] = array_merge($target[$key], $value);
-            } else {
-                $target[$key] = $value;
-            }
-        }
-        return $target;
-    }
+      if ($merge) {
+         $data = self::merge($this->data, $data);
+      }
 
-    final public function setData(array $data) {
-        $this->data = $data;
-        return $this;
-    }
-    final public function getData() {
-        return $this->data;
-    }
+      $this->setData($data);
+   }
+
+   final public function set($key, $value)
+   {
+      $this->data[$key] = $value;
+
+      return $this;
+   }
+
+   final public function get($key, $valueDefault = null)
+   {
+      return dig($this->data, $key, $valueDefault);
+   }
+
+   final public static function merge(array $source, array $target)
+   {
+      foreach ($source as $key => $value) {
+         if (isset($target[$key]) && is_array($value)) {
+            $target[$key] = array_merge($target[$key], $value);
+         } else {
+            $target[$key] = $value;
+         }
+      }
+
+      return $target;
+   }
+
+   final public function setData(array $data)
+   {
+      $this->data = $data;
+
+      return $this;
+   }
+
+   final public function getData()
+   {
+     return $this->data;
+   }
 }
