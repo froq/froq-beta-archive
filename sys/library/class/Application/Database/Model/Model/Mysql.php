2fb76f067fdd3ad526643e9f6605102f65ff8cd5
Kerem Güneş <qeremy@gmail.com>
2016-01-21 00:12:06 +0200

    format (use 3-tabs).

--- sys/library/class/Application/Database/Model/Model/Mysql.php
+++ sys/library/class/Application/Database/Model/Model/Mysql.php
@@ -5,18 +5,20 @@ use Application\Database\Vendor\Mysql as Database;
 
 final class Mysql extends Model
 {
-    final public function __construct(Database $db, $name, $primary) {
-        $this->db = $db;
-        $this->name = $name;
-        $this->primary = $primary;
-    }
+   final public function __construct(Database $db, $name, $primary) {
+      $this->db = $db;
+      $this->name = $name;
+      $this->primary = $primary;
+   }
 
-    final public function find() {
-        return $this->db->getConnection()->getAgent()->get(
-            "SELECT * FROM `{$this->name}` WHERE `{$this->primary}` = ?", [1] /* @todo [$this->id] */);
-    }
+   final public function find() {
+      return $this->db->getConnection()->getAgent()->get(
+         "SELECT * FROM `{$this->name}` WHERE `{$this->primary}` = ?", [1] /* @todo [$this->id] */);
+   }
 
-    final public function findAll() {}
-    final public function save() {}
-    final public function remove() {}
+   final public function findAll() {}
+
+   final public function save() {}
+
+   final public function remove() {}
 }
