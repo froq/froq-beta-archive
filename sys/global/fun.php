6ef7987ddf3499aafb1fb224e8bb082d0ee2cc9d
Kerem Güneş <qeremy@gmail.com>
2016-01-20 02:45:53 +0200

    nope.

--- sys/global/fun.php
+++ sys/global/fun.php
@@ -130,11 +130,11 @@ if (!function_exists('get_callee')) {
 function _prp($s) {
     $p = '';
     if (is_null($s)) {
-        $p .= 'NULL';
+        $p = 'NULL';
     } elseif (is_bool($s)) {
-        $p .= $s ? 'TRUE' : 'FALSE';
+        $p = $s ? 'TRUE' : 'FALSE';
     } else {
-        $p .= preg_replace('~\[(.+):(.+):(private|protected)\]~', '[\\1:\\3]', print_r($s, 1));
+        $p = preg_replace('~\[(.+):(.+):(private|protected)\]~', '[\\1:\\3]', print_r($s, 1));
     }
     return $p;
 }
