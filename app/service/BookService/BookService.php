b06a336ab06151a2fe8826e3cc06877baa10135d
Kerem Güneş <qeremy@gmail.com>
2016-01-21 05:21:07 +0200

    test findAll().

--- app/service/BookService/BookService.php
+++ app/service/BookService/BookService.php
@@ -13,9 +13,12 @@ class BookService extends Service
 
    public function main() {
       $this->model->id = 1; // get from request
-
       $book = $this->model->find();
       pre($book);
+
+      $books = $this->model->findAll();
+      $books = $this->model->findAll('id > ?', [0]);
+      pre($books);
 
       // $this->view('main');
       // $this->view->display('main');
