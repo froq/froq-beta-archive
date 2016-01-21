9976e44627252c2714c652db816ccaa01e0d2be7
Kerem Güneş <qeremy@gmail.com>
2016-01-21 05:39:15 +0200

    format (use 3-tabs).

--- sys/library/class/Application/Database/Database.php
+++ sys/library/class/Application/Database/Database.php
@@ -1,52 +1,50 @@
 <?php declare(strict_types=1);
 namespace Application\Database;
 
-use Application\Database\Vendor\Mysql,
-    Application\Database\Vendor\Couch,
-    Application\Database\Vendor\Mongo;
+use Application\Database\Vendor\{Mysql, Couch, Mongo};
 
 final class Database
 {
-    const VENDOR_MYSQL = 'mysql',
-          VENDOR_COUCH = 'couch',
-          VENDOR_MONGO = 'mongo';
+   const VENDOR_MYSQL = 'mysql',
+         VENDOR_COUCH = 'couch',
+         VENDOR_MONGO = 'mongo';
 
-    private static $instances = [];
+   private static $instances = [];
 
-    final public function __construct() {}
+   final public function __construct() {}
 
-    final public static function init(string $vendor)
-    {
-        if (isset(self::$instances[$vendor])) {
-            return self::$instances[$vendor];
-        }
-        $app = app();
-        $db = null;
-        $dbConfig = $app->config->get('db');
-        switch ($vendor) {
-            case self::VENDOR_MYSQL:
-                if (!isset($dbConfig[self::VENDOR_MYSQL][$app->env])) {
-                    throw new \Exception('MySQL options not found in config!');
-                }
-                $db = Mysql::init($dbConfig[self::VENDOR_MYSQL][$app->env]);
-                break;
-            default:
-                throw new \Exception('Unimplemented vendor given!');
-        }
+   final public static function init(string $vendor)
+   {
+      if (isset(self::$instances[$vendor])) {
+         return self::$instances[$vendor];
+      }
+      $app = app();
+      $db = null;
+      $dbConfig = $app->config->get('db');
+      switch ($vendor) {
+         case self::VENDOR_MYSQL:
+            if (!isset($dbConfig[self::VENDOR_MYSQL][$app->env])) {
+               throw new \Exception('MySQL options not found in config!');
+            }
+            $db = Mysql::init($dbConfig[self::VENDOR_MYSQL][$app->env]);
+            break;
+         default:
+            throw new \Exception('Unimplemented vendor given!');
+      }
 
-        return (self::$instances[$vendor] = $db);
-    }
+      return (self::$instances[$vendor] = $db);
+   }
 
-    final public function initMysql(): Mysql
-    {
-        return self::init(self::VENDOR_MYSQL);
-    }
-    final public function initCouch(): Couch
-    {
-        return self::init(self::VENDOR_COUCH);
-    }
-    final public function initMongo(): Mongo
-    {
-        return self::init(self::VENDOR_MONGO);
-    }
+   final public function initMysql(): Mysql
+   {
+      return self::init(self::VENDOR_MYSQL);
+   }
+   final public function initCouch(): Couch
+   {
+      return self::init(self::VENDOR_COUCH);
+   }
+   final public function initMongo(): Mongo
+   {
+      return self::init(self::VENDOR_MONGO);
+   }
 }
