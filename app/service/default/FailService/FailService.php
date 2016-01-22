a40d8e9f2f50739e212c8019ab8a7df1af36e848
Kerem Güneş <qeremy@gmail.com>
2016-01-22 03:12:26 +0200

    format (3-tabs).

--- app/service/default/FailService/FailService.php
+++ app/service/default/FailService/FailService.php
@@ -3,11 +3,12 @@ use Application\Service\Protocol\Site as Service;
 
 class FailService extends Service
 {
-    public function main() {
-        if ($this->viewData['fail']['code'] == 404) {
-            $data['error'] = '404 Not Found';
-            $data['error_detail'] = $this->viewData['fail']['text'];
-            $this->view('./app/service/view/fail/404', $data);
-        }
-    }
+   public function main()
+   {
+      if ($this->viewData['fail']['code'] == 404) {
+         $data['error'] = '404 Not Found';
+         $data['error_detail'] = $this->viewData['fail']['text'];
+         $this->view('./app/service/view/fail/404', $data);
+      }
+   }
 }
--- app/service/default/MainService/MainService.php
+++ app/service/default/MainService/MainService.php
@@ -3,8 +3,8 @@ use Application\Service\Protocol\Site as Service;
 
 class MainService extends Service
 {
-    public function main(): string
-    {
-        return 'Hello, Froq!';
-    }
+   public function main()
+   {
+      return 'Hello, Froq!';
+   }
 }
