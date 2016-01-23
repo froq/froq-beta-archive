32703346c0f56f2dc48c26ba89255f8f9760c154
Kerem Güneş <qeremy@gmail.com>
2016-01-23 21:37:38 +0200

    add license, docs.

--- sys/library/class/Application/Database/Model/Stack/Agent/Mysql.php
+++ sys/library/class/Application/Database/Model/Stack/Agent/Mysql.php
@@ -1,18 +1,61 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * Copyright (c) 2016 Kerem Güneş
+ *    <http://qeremy.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
 namespace Application\Database\Model\Stack\Agent;
 
 use Application\Database\Vendor\Vendor as Database;
 use Application\Database\Model\Stack\{Stack, StackInterface};
 
+/**
+ * @package    Application
+ * @subpackage Application\Database\Model\Stack\Agent
+ * @object     Application\Database\Model\Stack\Agent\Mysql
+ * @author     Kerem Güneş <qeremy@gmail.com>
+ */
 final class Mysql extends Stack
 {
-   final public function __construct(Database $db, $name, $primary) {
+   /**
+    * Constructor.
+    *
+    * @param Application\Database\Vendor\Vendor $db
+    * @param string                             $name
+    * @param string                             $primary
+    */
+   final public function __construct(Database $db, $name, $primary)
+   {
       $this->db = $db;
       $this->name = trim($name);
       $this->primary = trim($primary);
    }
 
-   public function find($primaryValue = null) {
+   /**
+    * Find an object.
+    *
+    * @param  string $primaryValue
+    * @return mixed
+    */
+   public function find($primaryValue = null)
+   {
       $primaryName = $this->primary;
       if ($primaryValue === null) {
          $primaryValue = $this->data[$primaryName];
@@ -26,7 +69,18 @@ final class Mysql extends Stack
          "SELECT * FROM `{$this->name}` WHERE `{$primaryName}` = ?", [$primaryValue]);
    }
 
-   public function findAll(string $where = null, array $params = null, $limit = null, int $order = -1) {
+   /**
+    * Find all object.
+    *
+    * @param  string    $where
+    * @param  array     $params
+    * @param  int|array $limit
+    * @param  int       $order
+    * @return mixed
+    */
+   public function findAll(string $where = null, array $params = null,
+      $limit = null, int $order = -1)
+   {
       $agent = $this->db->getConnection()->getAgent();
 
       if (empty($where)) {
@@ -46,7 +100,19 @@ final class Mysql extends Stack
       return $agent->getAll($query, $params);
    }
 
-   public function save() {}
+   /**
+    * Save an object.
+    *
+    * @return int|bool
+    */
+   public function save()
+   {}
 
-   public function remove() {}
+   /**
+    * Remove an object.
+    *
+    * @return int|bool
+    */
+   public function remove()
+   {}
 }
