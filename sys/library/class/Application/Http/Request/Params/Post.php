a9012bfdda80abf3386c5538f7fbb86c589f8fae
Kerem Güneş <qeremy@gmail.com>
2016-01-23 21:47:17 +0200

    add Application\Http\Request\Params\Post.

---
+++ sys/library/class/Application/Http/Request/Params/Post.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+namespace Application\Http;
+
+use Application\Util\Collection;
+
+/**
+ * @package    Application
+ * @subpackage Application\Http\Request\Params
+ * @object     Application\Http\Request\Params\Post
+ * @extends    Application\Util\Collection
+ * @author     Kerem Güneş <qeremy@gmail.com>
+ */
+final class Post
+   extends Collection
+{
+   /**
+    * Constructor.
+    *
+    * @param array $data
+    */
+   final public function __construct(array $data = [])
+   {
+      if (empty($data)) {
+         $data = $_POST;
+      }
+      parent::__construct($data);
+   }
+}
