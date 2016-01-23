68041ca4e6bd1b0c36e6533b497a12aa8835c5b7
Kerem Güneş <qeremy@gmail.com>
2016-01-23 06:53:23 +0200

    format.

--- sys/library/class/Application/Service/ServiceInterface.php
+++ sys/library/class/Application/Service/ServiceInterface.php
@@ -4,15 +4,15 @@ namespace Application\Service;
 interface ServiceInterface
 {
    const PROTOCOL_SITE       = 'site',
-      	PROTOCOL_REST       = 'rest';
+         PROTOCOL_REST       = 'rest';
 
    const SERVICE_NAME_SUFFIX = 'Service',
-      	SERVICE_MAIN        = 'MainService',
-      	SERVICE_FAIL        = 'FailService';
+         SERVICE_MAIN        = 'MainService',
+         SERVICE_FAIL        = 'FailService';
 
    const METHOD_NAME_PREFIX  = 'do',
-      	METHOD_INIT         = 'init',
-      	METHOD_MAIN         = 'main',
-      	METHOD_ONBEFORE     = 'onbefore',
-      	METHOD_ONAFTER      = 'onafter';
+         METHOD_INIT         = 'init',
+         METHOD_MAIN         = 'main',
+         METHOD_ONBEFORE     = 'onbefore',
+         METHOD_ONAFTER      = 'onafter';
 }
