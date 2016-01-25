9847d6124b0fbe657441e12257cf2e1262a615ec
Kerem Güneş <qeremy@gmail.com>
2016-01-26 00:14:34 +0200

    add doc.

--- app/global/def.php
+++ app/global/def.php
@@ -1,4 +1,4 @@
 <?php defined('root') or die('Access denied!');
 /**
- * Create your functions here that will be used globally in app.
+ * Create your constants here that will be used globally in app.
  */
