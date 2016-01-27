c77ee5ec944b0ab8df31642a7f3cdcd3200d2c91
Kerem Güneş <qeremy@gmail.com>
2016-01-27 11:57:08 +0200

    add license, docs.

--- sys/library/class/Application/Database/Database.php
+++ sys/library/class/Application/Database/Database.php
@@ -1,48 +1,110 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * Copyright (c) 2016 Kerem Güneş
+ *    <http://qeremy.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
 namespace Application\Database;
 
 use Application\Database\Vendor\{Mysql, Couch, Mongo};
 
+/**
+ * @package    Application
+ * @subpackage Application\Database
+ * @object     Application\Database\Database
+ * @author     Kerem Güneş <qeremy@gmail.com>
+ */
 final class Database
 {
+   /**
+    * Vendor names.
+    * @const string
+    */
    const VENDOR_MYSQL = 'mysql',
          VENDOR_COUCH = 'couch',
          VENDOR_MONGO = 'mongo';
 
+   /**
+    * Database vendor instances.
+    * @var array
+    */
    private static $instances = [];
 
-   final public function __construct() {}
+   // final public function __construct() {}
 
+   /**
+    * Instance creator.
+    *
+    * @param  string $vendor
+    * @return Application\Database\Vendor\{Mysql, Couch, Mongo}
+    * @throws \Exception
+    */
    final public static function init(string $vendor)
    {
       if (isset(self::$instances[$vendor])) {
          return self::$instances[$vendor];
       }
+
       $app = app();
-      $db = null;
-      $dbConfig = $app->config->get('db');
+      $cfg = $app->config->get('db');
+      if (!isset($cfg[$vendor][$app->env])) {
+         throw new \Exception("`{$vendor}` options not found in config!");
+      }
+
       switch ($vendor) {
+         // only mysql for now
          case self::VENDOR_MYSQL:
-            if (!isset($dbConfig[self::VENDOR_MYSQL][$app->env])) {
-               throw new \Exception('MySQL options not found in config!');
-            }
-            $db = Mysql::init($dbConfig[self::VENDOR_MYSQL][$app->env]);
+            self::$instances[$vendor] = Mysql::init($cfg[$vendor][$app->env]);
             break;
          default:
             throw new \Exception('Unimplemented vendor given!');
       }
 
-      return (self::$instances[$vendor] = $db);
+      return self::$instances[$vendor];
    }
 
+   /**
+    * Create a MySQL worker instance.
+    *
+    * @return Application\Database\Vendor\Mysql
+    */
    final public function initMysql(): Mysql
    {
       return self::init(self::VENDOR_MYSQL);
    }
+
+   /**
+    * Create a CouchDB worker instance.
+    *
+    * @return Application\Database\Vendor\Couch
+    */
    final public function initCouch(): Couch
    {
       return self::init(self::VENDOR_COUCH);
    }
+
+   /**
+    * Create a MongoDB worker instance.
+    *
+    * @return Application\Database\Vendor\Mongo
+    */
    final public function initMongo(): Mongo
    {
       return self::init(self::VENDOR_MONGO);
