df00b6bf05684a44ec064f4623bc83ae6885f851
Kerem Güneş <qeremy@gmail.com>
2016-01-28 20:26:58 +0200

    complete error handler.

--- sys/library/class/Application/Handler/Error.php
+++ sys/library/class/Application/Handler/Error.php
@@ -5,6 +5,53 @@ final class Error
 {
    final public static function handler()
    {
-      return function() {};
+      return function($ecode, $emesg, $efile, $eline) {
+         // error was suppressed with the @-operator
+         if (!$ecode || !($ecode & error_reporting())) {
+            return;
+         }
+
+         $error = null;
+         // check error type
+         switch ($ecode) {
+            case E_ERROR:
+            case E_PARSE:
+            case E_CORE_ERROR:
+            case E_CORE_WARNING:
+            case E_COMPILE_ERROR:
+            case E_COMPILE_WARNING:
+            case E_STRICT:
+               $error = sprintf('Runtime error in %s:%s ecode[%s] emesg[%s]',
+                  $efile, $eline,  $ecode, $emesg);
+               break;
+            case E_RECOVERABLE_ERROR:
+               $error = sprintf('E_RECOVERABLE_ERROR in %s:%s ecode[%s] emesg[%s]',
+                  $efile, $eline, $ecode, $emesg);
+               break;
+            case E_USER_ERROR:
+               $error = sprintf('E_USER_ERROR in %s:%s ecode[%s] emesg[%s]',
+                  $efile, $eline, $ecode, $emesg);
+               break;
+            case E_USER_WARNING:
+               $error = sprintf('E_USER_WARNING in %s:%s ecode[%s] emesg[%s]',
+                  $efile, $eline, $ecode, $emesg);
+               break;
+            case E_USER_NOTICE:
+               $error = sprintf('E_USER_NOTICE in %s:%s ecode[%s] emesg[%s]',
+                  $efile, $eline, $ecode, $emesg);
+               break;
+            default:
+               $error = sprintf('Unknown error in %s:%s ecode[%s] emesg[%s]',
+                  $efile, $eline, $ecode, $emesg);
+         }
+
+         // throw! exception handler will catch it
+         if ($error) {
+            throw new \ErrorException($error, $ecode);
+         }
+
+         // don't execute php internal error handler
+         return true;
+      };
    }
 }
