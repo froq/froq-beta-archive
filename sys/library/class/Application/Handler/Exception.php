3ee20a89a581b86fed117a72e1e117e9df6a2ff1
Kerem Güneş <qeremy@gmail.com>
2016-01-29 19:17:41 +0200

    complete error handling stuff.

--- sys/library/class/Application/Handler/Exception.php
+++ sys/library/class/Application/Handler/Exception.php
@@ -6,6 +6,12 @@ final class Exception
    final public static function handler()
    {
       return function(\Throwable $e) {
+         // if not local no error display (so set & store option)
+         if (!is_local() or 1) {
+            set_global('display_errors', ini_set('display_errors', '0'));
+         }
+
+         // will be catched in shutdown handler
          throw $e;
       };
    }
--- sys/library/class/Application/Handler/Shutdown.php
+++ sys/library/class/Application/Handler/Shutdown.php
@@ -9,13 +9,23 @@ final class Shutdown
          $error = error_get_last();
          if (isset($error['type']) && $error['type'] == E_ERROR) {
             // pre($error);
-            $error = sprintf('Shutdown! E_ERROR in %s:%d errno[%d] errmsg[%s]',
+            $error = sprintf('Shutdown! E_ERROR in %s:%d ecode[%d] emesg[%s]',
                $error['file'], $error['line'], $error['type'], $error['message']);
 
-            // @todo
+            // easy boy!
             $app = app();
             $app->logger->logFail($error);
+
+            // handle response properly
             $app->response->setStatus(500);
+            $app->response->setContentType('none');
+            $app->response->send();
+
+            // reset error display option
+            $opt = get_global('display_errors');
+            if ($opt !== null) {
+               ini_set('display_errors', $opt);
+            }
          }
       };
    }
