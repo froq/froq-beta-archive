02f06a0e634ec717599dda2c7177c3be3e2d6ca1
Kerem Güneş <qeremy@gmail.com>
2016-01-29 20:47:11 +0200

    add license, docs.

--- sys/library/class/Application/Util/View.php
+++ sys/library/class/Application/Util/View.php
@@ -1,64 +1,167 @@
-<?php namespace Application\Util;
+<?php
+/**
+ * Copyright (c) 2016 Kerem Güneş
+ *    <http://qeremy.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Application\Util;
 
 use Application\Service\ServiceInterface;
 
+/**
+ * @package    Application
+ * @subpackage Application\Util
+ * @object     Application\Util\View
+ * @author     Kerem Güneş <qeremy@gmail.com>
+ */
 final class View
 {
+   /**
+    * Partial files.
+    * @const string
+    */
    const PARTIAL_HEAD = 'partial/head',
         	PARTIAL_FOOT = 'partial/foot';
 
+   /**
+    * Application object.
+    * @var Application\Application
+    */
    private $app;
+
+   /**
+    * Service object.
+    * @var Application\Service\ServiceInterface
+    */
    private $service;
 
-   final public function __construct(ServiceInterface $service) {
+   /**
+    * Constructor.
+    *
+    * @param Application\Service\ServiceInterface $service
+    */
+   final public function __construct(ServiceInterface $service)
+   {
       $this->app = $service->app;
       $this->service = $service;
    }
 
-   final public function display($file, array $data = null) {
+   /**
+    * Display/render view file.
+    *
+    * @param  string $file
+    * @param  array  $data
+    * @return void
+    */
+   final public function display(string $file, array $data = null)
+   {
       $this->includeFile($this->prepareFile($file), $data);
    }
 
-   final public function displayHead(array $data = null) {
+   /**
+    * Display/render partial/header file.
+    *
+    * @param  array $data
+    * @return void
+    */
+   final public function displayHead(array $data = null)
+   {
       // check local service file
       $file = $this->prepareFile(self::PARTIAL_HEAD, false);
       if (!is_file($file)) {
          // look up for global service file
          $file = $this->prepareFileGlobal(self::PARTIAL_HEAD);
       }
+
       $this->includeFile($file, $data);
    }
-   final public function displayFoot(array $data = null) {
+
+   /**
+    * Display/render partial/footer file.
+    *
+    * @param  array $data
+    * @return void
+    */
+   final public function displayFoot(array $data = null)
+   {
       // check local service file
       $file = $this->prepareFile(self::PARTIAL_FOOT, false);
       if (!is_file($file)) {
          // look up for global service file
          $file = $this->prepareFileGlobal(self::PARTIAL_FOOT);
       }
+
       $this->includeFile($file, $data);
    }
 
-   final public function includeFile($file, array $data = null) {
+   /**
+    * Include file.
+    *
+    * @param  string $file
+    * @param  array  $data
+    * @return void
+    */
+   final public function includeFile(string $file, array $data = null)
+   {
       extract((array) $data);
+
       include($file);
    }
-   final public function prepareFile($file, $fileCheck = true) {
+
+   /**
+    * Prepare file path.
+    *
+    * @param  string $file
+    * @param  bool   $fileCheck
+    * @return string
+    */
+   final public function prepareFile(string $file, bool $fileCheck = true)
+   {
       // default file given
       if ($file[0] == '.') {
          $file = sprintf('%s.php', $file);
       } else {
          $file = sprintf('./app/service/%s/view/%s.php', $this->service->name, $file);
       }
+
       if ($fileCheck && !is_file($file)) {
          throw new \RuntimeException('View file not found! file: '. $file);
       }
+
       return $file;
    }
-   final public function prepareFileGlobal($file, $fileCheck = true) {
+
+   /**
+    * Prepare global file path.
+    *
+    * @param  string $file
+    * @param  bool   $fileCheck
+    * @return string
+    */
+   final public function prepareFileGlobal(string $file, bool $fileCheck = true)
+   {
       $file = sprintf('./app/service/view/%s.php', $file);
       if ($fileCheck && !is_file($file)) {
          throw new \RuntimeException('View file not found! file: '. $file);
       }
+
       return $file;
    }
 }
